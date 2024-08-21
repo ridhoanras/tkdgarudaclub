52ea36c51ed5e62a5eb9d1b0c538aeb383f626e3
Your Name <you@example.com>
2024-08-21 09:33:12 +0700

    first commit

--- src/components/Home/featureSection.js
+++ src/components/Home/featureSection.js
@@ -34,9 +34,9 @@ const FeatureSection = () => {
 
       <div className="max-w-7xl mx-auto lg:px-8 md:px-3">
         <div className="mx-auto max-w-7xl px-4 sm:mt-10 sm:px-6 md:mt-10 lg:mt-10 lg:px-0 xl:mt-10 flex flex-col lg:flex-row gap-3 lg:flex-justify">
-          <div className="lg:w-1/2 my-4">
+          <div className="lg:w-1/2 my-4 flex justify-center">
             <img
-              className="rounded-xl h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+              className="rounded-xl h-56 w-100 object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
               src={HeroImage}
               alt="HeroImage"
             ></img>
--- src/components/about/aboutHeader.js
+++ src/components/about/aboutHeader.js
@@ -7,7 +7,7 @@ const AboutHeader = () => {
   return (
     <Fade>
       <div className="max-w-7xl mx-auto mt-10">
-        <div className="overflow-hidden rounded-xl max-h-96 m-4">
+        <div className="overflow-hidden rounded-x1 max-h-96 m-4">
           <img alt="Image" src={Image3}></img>
         </div>
         <div className="mt-10 px-8">
