9c9257b22caf275f66ac2180a9454f2aa6eda840
Your Name <you@example.com>
2024-08-31 16:33:56 +0700

    first commit

---
+++ src/blog/pelatihan-taekwondo-garuda-club-penguasaan-semangat-dalam-gerakan.md
@@ -0,0 +1,37 @@
+---
+templateKey: blog-post
+title: Pelatihan Taekwondo Garuda Club Penguasaan Semangat Dalam Gerakan
+date: 2024-05-22T15:02:06.069Z
+featuredimage: /img/pelatihan-taekwondo-garuda-club-penguasaan-semangat-dalam-gerakan.jpg
+categories:
+  - blog
+tags:
+  - Pelatihan Taekwondo
+  - Teknik Taekwondo
+  - Bela diri
+  - Gerakan Taekwondo
+  - Semangat Taekwondo
+  - Disiplin bela diri
+  - Keterampilan fisik
+  - Kekuatan mental
+  - Klub Taekwondo
+  - Kompetisi Taekwondo
+  - Pengembangan karakter
+  - Filosofi Taekwondo
+  - Pelatihan bela diri
+  - Latihan fisik
+Penguasaan teknik
+description: Dalam dunia bela diri, Taekwondo tidak hanya tentang teknik dan kekuatan fisik, tetapi juga tentang penguasaan diri dan semangat yang menggerakkan setiap langkah. Di Garuda Club, pelatihan Taekwondo dihadirkan dengan pendekatan yang unik, memadukan kekuatan fisik dan mental untuk mencapai puncak kemampuan seorang praktisi.
+---
+## Filosofi di Balik Gerakan 
+
+Dalam dunia bela diri, Taekwondo tidak hanya tentang teknik dan kekuatan fisik, tetapi juga tentang penguasaan diri dan semangat yang menggerakkan setiap langkah. Di Garuda Club, pelatihan Taekwondo dihadirkan dengan pendekatan yang unik, memadukan kekuatan fisik dan mental untuk mencapai puncak kemampuan seorang praktisi. Garuda Club meyakini bahwa setiap gerakan dalam Taekwondo harus diiringi dengan semangat yang kuat. Instruktur kami tidak hanya melatih para siswa untuk menguasai teknik tendangan, pukulan, dan pertahanan, tetapi juga membimbing mereka untuk menemukan kekuatan dalam diri mereka. Dengan menanamkan nilai-nilai disiplin, keberanian, dan kepercayaan diri, setiap siswa diajak untuk mengembangkan potensi maksimal mereka.
+
+## Program Pelatihan yang Terstruktur Pengembangan Mental dan Fisik
+
+Program pelatihan di Garuda Club dirancang secara bertahap, dimulai dari pengenalan dasar-dasar Taekwondo hingga teknik lanjutan yang membutuhkan ketangkasan tinggi. Setiap tingkatan pelatihan disusun agar para siswa dapat menguasai teknik dengan baik sebelum melangkah ke tahap berikutnya. Fokus utama kami adalah memastikan bahwa setiap gerakan dilakukan dengan sempurna, didorong oleh semangat yang tidak pernah pudar. Pelatihan di Garuda Club tidak hanya menitikberatkan pada perkembangan fisik, tetapi juga pada pengembangan mental. Kami percaya bahwa seorang atlet Taekwondo yang sejati harus memiliki ketenangan pikiran dan kekuatan mental yang seimbang dengan keterampilan fisiknya. Sesi meditasi, pengendalian napas, dan refleksi diri menjadi bagian integral dari pelatihan, membantu siswa untuk tetap fokus dan tangguh dalam setiap situasi.
+
+
+## Keberhasilan dalam Kompetisi Mencapai Penguasaan Melalui Dedikasi
+
+Sejumlah siswa Garuda Club telah menunjukkan prestasi luar biasa di berbagai kompetisi, baik tingkat lokal maupun nasional. Keberhasilan mereka tidak hanya ditentukan oleh kekuatan fisik, tetapi juga oleh semangat dan determinasi yang telah mereka pelajari selama pelatihan. Setiap kemenangan adalah cerminan dari dedikasi dan kerja keras, serta semangat yang tidak pernah padam. Di Garuda Club, kami percaya bahwa penguasaan sejati dalam Taekwondo hanya dapat dicapai melalui dedikasi yang penuh. Setiap siswa didorong untuk tidak hanya menguasai teknik, tetapi juga memahami dan menghargai nilai-nilai yang diajarkan. Dengan semangat yang menggerakkan setiap gerakan, mereka akan siap menghadapi tantangan apapun, baik di dalam maupun di luar arena. Pelatihan di Garuda Club bukan hanya tentang belajar Taekwondo, tetapi juga tentang mengembangkan karakter dan semangat yang akan mengiringi setiap langkah dalam hidup. Di sini, para siswa diajarkan untuk menguasai tidak hanya gerakan, tetapi juga semangat yang menjadi inti dari setiap keberhasilan.
--- src/components/Home/featureSection.js
+++ src/components/Home/featureSection.js
@@ -120,7 +120,8 @@ const FeatureSection = () => {
       </Fade>
 
       <div className="max-w-7xl mx-auto lg:px-8 md:px-3">
-        <div className="mx-auto max-w-7xl px-4 sm:mt-10 sm:px-6 md:mt-10 lg:mt-10 lg:px-0 xl:mt-10 flex flex-col lg:flex-row gap-3 lg:flex-justify">
+        <div className="mx-auto max-w-7xl px-4 sm:mt-10 sm:px-6 md:mt-10 lg:mt-10 lg:px-0 xl:mt-10 flex 
+        flex-col lg:flex-row gap-3 lg:flex-justify">
           <div className="lg:w-1/2 my-4 flex justify-center">
             <img
               className="rounded-xl h-56 w-100 object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
@@ -131,11 +132,13 @@ const FeatureSection = () => {
 
           <div className="lg:w-1/2 sm:text-center lg:text-right flex flex-col justify-center">
             <h2 className="text-black text-4xl font-semibold">MENGAPA</h2>
-            <h3 className="text-black text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">
+            <h3 className="text-black text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl 
+            lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">
               Latihan di sini!
             </h3>
 
-            <p className="mt-3 text-base text-black-70 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-auto lg:mr-0">
+            <p className="mt-3 text-base text-black-70 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 
+            md:text-xl lg:mx-auto lg:mr-0">
               Di Garuda Club, akan dilatih oleh pelatih bersertifikat yang memiliki pengalaman luas 
               dalam taekwondo. Mereka tidak hanya ahli dalam teknik tetapi juga dalam menginspirasi dan
               memotivasi peserta.
@@ -145,7 +148,10 @@ const FeatureSection = () => {
               <div className="rounded-md">
                 <a
                   href="tel:#"
-                  className="transition-all duration-500ms ease-in-out hover:ease-in-out w-full flex items-center justify-center px-8 py-3 text-base font-medium rounded-md text-center bg-purple text-white hover:text-black border border-purple hover:bg-transparent md:text-lg md:px-10"
+                  className="transition-all duration-500ms ease-in-out hover:ease-in-out w-full flex 
+                  items-center justify-center px-8 py-3 text-base font-medium rounded-md text-center 
+                  bg-purple text-white hover:text-black border border-purple hover:bg-transparent 
+                  md:text-lg md:px-10"
                 >
                   Hubungi kami sekarang
                 </a>
@@ -153,7 +159,10 @@ const FeatureSection = () => {
               <div className="mt-3 sm:mt-0 sm:ml-3">
                 <a
                   href="/gallery"
-                  className="transition-all duration-500ms ease-in-out hover:ease-in-out w-full flex items-center justify-center px-8 py-3 border border-purple text-base font-medium rounded-md text-black hover:text-white bg-transparent hover:bg-purple md:py-3 md:text-lg md:px-10"
+                  className="transition-all duration-500ms ease-in-out hover:ease-in-out w-full flex 
+                  items-center justify-center px-8 py-3 border border-purple text-base font-medium 
+                  rounded-md text-black hover:text-white bg-transparent hover:bg-purple md:py-3 
+                  md:text-lg md:px-10"
                 >
                   Lihat Galeri
                   <svg
@@ -180,7 +189,8 @@ const FeatureSection = () => {
 
       <Fade bottom cascade>
         <div className="mt-10 flex flex-row justify-space xxs:flex-col xs:flex-col sm:flex-row">
-          <div className="w-2/6 bg-gradient-to-r from-pink to-purple p-8 rounded-xl m-5 xxs:w-full xs:w-full sm:w-2/6">
+          <div className="w-2/6 bg-gradient-to-r from-pink to-purple p-8 rounded-xl m-5 xxs:w-full 
+          xs:w-full sm:w-2/6">
             <span className="text-black font-semibold font-montserrat text-4xl">
               MANFAAT
             </span>
@@ -191,7 +201,8 @@ const FeatureSection = () => {
               </div>
               <div className="mt-5">
                 <p className="text-black text-2xl">3. Menambah Percaya Diri dan Keberanian</p>
-                <p className="text-black opacity-50 text-sm">4. Meningkatkan Koordinasi dan Keseimbangan</p>
+                <p className="text-black opacity-50 text-sm">4. Meningkatkan Koordinasi dan 
+                  Keseimbangan</p>
               </div>
               <div className="mt-5">
                 <p className="text-black text-2xl">5. Meningkatkan Ketahanan Tubuh</p>
@@ -208,9 +219,10 @@ const FeatureSection = () => {
             Mulai membangun prestasi di Taekwondo Garuda Club yang luar biasa
             </h2>
             <p className="mt-10 text-white opacity-70">
-            Taekwondo Garuda Club, tempat di mana potensi Anak Ayah dan Bunda bertemu dengan peluang tak terbatas untuk
-            membangun prestasi luar biasa! Di sini, kami tidak hanya mengajarkan teknik dan keterampilan 
-            bela diri; kami membentuk karakter dan menginspirasi prestasi yang mengesankan.
+            Taekwondo Garuda Club, tempat di mana potensi Anak Ayah dan Bunda bertemu dengan peluang tak 
+            terbatas untuk membangun prestasi luar biasa! Di sini, kami tidak hanya mengajarkan teknik 
+            dan keterampilan bela diri; kami membentuk karakter dan menginspirasi prestasi yang 
+            mengesankan.
             </p>
           </div>
         </div>
@@ -220,7 +232,8 @@ const FeatureSection = () => {
         <h2 className="text-black text-4xl font-semibold opacity-70">
           MEMBUKA PENDAFTARAN
         </h2>
-        <h3 className="text-black text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">
+        <h3 className="text-black text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl 
+        text-gradient bg-gradient-to-r from-pink to-purple">
           Hubungi Admin Untuk Menjadi Anggota 
         </h3>
         <form onSubmit={handleSubmit} className="space-y-4 mt-6">
@@ -298,7 +311,8 @@ const FeatureSection = () => {
         </div>
         <button
           type="submit"
-          className="w-full px-4 py-2 bg-gradient-to-r from-pink to-purple text-white rounded-md hover:bg-gradient-to-l hover:from-purple hover:to-pink"
+          className="w-full px-4 py-2 bg-gradient-to-r from-pink to-purple text-white rounded-md 
+          hover:bg-gradient-to-l hover:from-purple hover:to-pink"
         >
           Kirim Pesan
         </button>
@@ -309,10 +323,12 @@ const FeatureSection = () => {
   <h2 className="text-black text-4xl font-semibold opacity-70">
     INTEGRASI
   </h2>
-  <h3 className="text-black text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">
+  <h3 className="text-black text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl 
+  text-gradient bg-gradient-to-r from-pink to-purple">
     Kami Terintegrasi dengan unit
   </h3>
-  <p className="text-black opacity-70 font-normal mt-3 text-xs xxs:text-xs xs:text-xs sm:text-xs md:text-sm lg:text-lg"></p>
+  <p className="text-black opacity-70 font-normal mt-3 text-xs xxs:text-xs xs:text-xs sm:text-xs 
+  md:text-sm lg:text-lg"></p>
   
   {/* <!-- Logo Container --> */}
   <div className="logo-container mt-5 flex space-x-4">
@@ -331,7 +347,9 @@ const FeatureSection = () => {
     <div className="rounded-md">
       <a
         href="/contact/"
-        className="transition-all duration-500ms ease-in-out hover:ease-in-out w-full flex items-center justify-center px-8 py-3 text-base font-medium rounded-md text-center bg-purple text-white hover:text-black border border-purple hover:bg-transparent md:text-lg md:px-10"
+        className="transition-all duration-500ms ease-in-out hover:ease-in-out w-full flex items-center 
+        justify-center px-8 py-3 text-base font-medium rounded-md text-center bg-purple text-white 
+        hover:text-black border border-purple hover:bg-transparent md:text-lg md:px-10"
       >
         Kirim Pertanyaan
       </a>
