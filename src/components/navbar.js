55922457d74ae1f444b06a9e641f0625c286a239
Ridho Anras <55146436+ridhoanras@users.noreply.github.com>
2026-04-19 00:12:15 +0700

    Update navbar link to use placeholder URL
    Changed the navbar link from an external URL to a placeholder.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -74,7 +74,7 @@ const Navbar = () => {
                 </Link>
                 <div className="items-center">
                   <a
-                    href="https://eksashop.web.id"
+                    href="#"
                     className="transition-all duration-500ms ease-in-out 
                     hover:ease-in-out bg-transparent mt-5 py-2.5 px-4 
                     text-base font-medium text-center text-black rounded-lg 
